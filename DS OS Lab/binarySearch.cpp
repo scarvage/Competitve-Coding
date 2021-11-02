206e7377d391decd18c5f55f1e20a81f49f6ef86
scarvage <mojha001@gmail.com>
2021-11-02 12:54:01 +0530

    binary search added

---
+++ DS OS Lab/binarySearch.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int binarySearch(int arr[],int low,int high,int key)
+{
+    int mid = low + (high-low)/2;
+
+    if(low>high)
+    {
+        return -1;
+    }
+    if(arr[mid] == key)
+    {
+        return mid; 
+    }
+    else if(arr[mid]>key)
+    {
+        return binarySearch(arr,low,mid-1,key);
+    }else
+    {
+        return binarySearch(Arr,mid+1,high,key);
+    }
+}
+int main() {
+
+    //Size of array
+    int n;
+    cin>>n;
+
+    //indexing
+    int low=0;
+    int high = n-1;
+
+    //taking in elements of array
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    //taking input of the number to find
+    int key;
+    cin>> key;
+
+    // sorting the array as binary search only applicable on sorted arrays
+    int size = size(arr)
+    sort(arr,arr+size);
+
+    int ans = binarySearch(arr,low,high,key);
+
+
+    if (ans!=-1)
+    {
+        cout<<"The number is present in the Array";
+    }else
+    {
+        cout<<"The number is not present";
+    }
+   
+}
---
+++ DS OS Lab/linearSearch.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int linearSearch(int arr[],int l,int n,int key)
+{
+    //if index goes out of size
+    if (n<l)
+    {
+        return -1;
+    }
+    //if the key is found
+    if(arr[l]==key)
+    {
+        return l;
+    }
+    //incrementing the value of index and again following the step
+    return linearSearch(arr,l+1,n,key);
+    
+}
+
+
+int main() {
+    //Size of array
+    int n;
+    cin>>n;
+
+    //indexing set to 0
+    int l=0;
+
+    //taking in elements of array
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    //taking input of the number to find
+    int key;
+    cin>> key;
+    
+
+    int ans = linearSearch(arr,l,n,key);
+
+    if (ans!=-1)
+    {
+        cout<<"The number is present in the Array";
+    }else
+    {
+        cout<<"The number is not present";
+    }
+    
+}
